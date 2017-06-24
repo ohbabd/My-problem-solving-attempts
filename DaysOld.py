ada89fe5cd5599103454dfc48371dd1d0b0f97d4
Abdou <ohbabd@gmail.com>
2017-06-24 02:43:14 +0000

    update Daysold.py
    Added and edited some comments

--- DaysOld.py
+++ DaysOld.py
@@ -1,6 +1,8 @@
 #This script calculates how many days are there between to given dates
 #Dates are supposed correct
 
+# calculates how many leap years
+#there are between the two dates.
 def leapYear(year1,year2):
     leap = 0
     while year1 <= year2:
@@ -10,11 +12,12 @@ def leapYear(year1,year2):
                 leap = leap - 1
         year1 = year1 + 1
     return leap
-# calculates how many leap years
-#there are between the two dates.
 
+#Array to store month days
 monthsDays = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+# calculates how many days we must not include before
+#the first date and after the second date
 def daysLived(month,day):
     i = 1
     livedDays = 0
@@ -22,15 +25,13 @@ def daysLived(month,day):
         livedDays = livedDays + monthsDays[i]
         i = i + 1
     return livedDays + day - 1
-# calculates how many days you didn't
-#live in the year of your birth, and
-#the day you lived in the current year .
 
+#Calculates das between to given dates
 def daysBetweenDates(year1,month1,day1,year2,month2,day2):
-    days = (year2 - year1) * 365 + leapYear(year1,year2) #total days between the 2 years
-    days2sub = daysLived(month1,day1)                    #days you didn't live in your birth year
-    days2add = daysLived(month2,day2)                    #days you lived in teh current year
+    days = (year2 - year1) * 365 + leapYear(year1,year2) #Total days between the 2 years
+    days2sub = daysLived(month1,day1)                    #Days to exlude
     days = days + days2add - days2sub
+    
     if month2 == 2:             # If it's a leap year and month2 is February, we
         days = days - 1         #will add 2 leap days instead of one, so here we
                                 #substract 1 day if it's the case
