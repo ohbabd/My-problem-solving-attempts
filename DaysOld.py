18d0c74b0edeab3fe62539602c8470ff2c3a7741
Abdou <ohbabd@gmail.com>
2017-06-30 21:50:57 +0000

    Update DaysOld.py

--- DaysOld.py
+++ DaysOld.py
@@ -1,5 +1,5 @@
-# This script calculates how many days are there between to given dates                                    
-# Dates are supposed correct                                                                               
+# This script calculates how many days are there between two given dates                                    
+# Dates are supposed to be correct                                                                               
                                                                                                            
                                                                                                            
 monthsDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]   # Array to store days of months            
@@ -38,7 +38,7 @@ def days_between_dates(year1, month1, day1, year2, month2, day2):
                                                                                                            
     if month2 <= 2 and leap:                 # Also if the second date is before February 29th             
         days = days - 1                      # in a leap year we should not add a leap day.                
-        if day2 == 29:                       # And if the second date si exactly February 29th in          
+        if day2 == 29:                       # And if the second date is exactly February 29th in          
             days = days - 1                  # a leap year, we will add 2 leap days, so we subtract 1 here.
                                                                                                            
     return days                                                                                            
