11fe88207f29f567418a021c420233751d0062c0
Abdou <ohbabd@gmail.com>
2017-08-09 22:16:38 +0100

    Update cellular_automaton.py

--- cellular_automaton.py
+++ cellular_automaton.py
@@ -1,26 +1,18 @@
-def rule(n):
+def cellular_automaton(string, pattern, n):
     rules_numbers = [[128, 'xxx'], [64, 'xx.'], [32, 'x.x'], [16, 'x..'],
                      [8, '.xx'], [4, '.x.'], [2, '..x'], [1, '...']]
     rules = {'...': '.', '..x': '.', '.x.': '.', '.xx': '.', 'x..': '.', 'x.x': '.', 'xx.': '.', 'xxx': '.'}
-    while n != 0:
+    while pattern != 0:
         for r in rules_numbers:
-            if n - r[0] >= 0:
+            if pattern - r[0] >= 0:
                 rules[r[1]] = 'x'
-                n -= r[0]
+                pattern -= r[0]
                 break
-    return rules
-
-
-def cellular_automaton(string, pattern, n):
-    if n == 0:
-        return string
-    rules = rule(pattern)
-    ng = ""
-    for j in range(len(string)):
-        nxt = j + 1
-        if j == len(string) - 1:
-            nxt = 0
-        ng += rules[string[j - 1] + string[j] + string[nxt]]
-        j += 1
-    return cellular_automaton(ng, pattern, n - 1)
+    for i in range(n):
+        ng = ""
+        for j in range(len(string)):
+            ng += rules[string[j - 1] + string[j] + string[(j+1) % len(string)]]
+            j += 1
+        string = ng
+    return string
 
